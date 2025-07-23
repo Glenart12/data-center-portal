b65edd20d3215c68a92515ef4837536bdf1a045a
Lorenzo Barbara <lb@glenartgroup.com>
2025-07-23 11:49:50 -0500

    Simplify layout.js for testing

--- app/layout.js
+++ app/layout.js
@@ -1,43 +1,9 @@
-// import { UserProvider } from '@auth0/nextjs-auth0/client';
-// import Header from './components/Header';
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Century+Gothic:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body style={{
-        margin: 0,
-        padding: 0,
-        fontFamily: '"Century Gothic", CenturyGothic, AppleGothic, sans-serif',
-        backgroundImage: 'url("https://images.unsplash.com/photo-1558494949-ef010cbdcc31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2134&q=80")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed',
-        minHeight: '100vh'
-      }}>
-        {/* Background blur overlay */}
-        <div style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.85)',
-          backdropFilter: 'blur(8px)',
-          zIndex: -1
-        }} />
-        
-        {/* Temporarily disabled UserProvider and Header */}
-        <main style={{
-          position: 'relative',
-          zIndex: 1
-        }}>
-          {children}
-        </main>
+      <body>
+        <h1>TESTING - Simple Layout</h1>
+        {children}
       </body>
     </html>
   );
