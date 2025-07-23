fb4772ba2651572f5051b2a247de6147ef87200d
Lorenzo Barbara <lb@glenartgroup.com>
2025-07-23 13:55:04 -0500

    Updated background to Data-Centre-Stock-04.jpg

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
         margin: 0,
         padding: 0,
         fontFamily: '"Century Gothic", CenturyGothic, AppleGothic, sans-serif',
-        backgroundImage: 'url("https://images.unsplash.com/photo-1558494949-ef010cbdcc31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2134&q=80")',
+        backgroundImage: 'url("/Data-Centre-Stock-04.jpg")',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundAttachment: 'fixed',
