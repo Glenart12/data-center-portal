8c716b97a29c37a46ab815f65db60717bf588c93
Lorenzo Barbara <lb@glenartgroup.com>
2025-09-15 13:10:50 -0500

    Raised Risk Status donut chart higher for true centering

--- app/risk/page.js
+++ app/risk/page.js
@@ -483,7 +483,7 @@ function RiskPage() {
                         position: 'absolute',
                         width: '180px',
                         height: '180px',
-                        top: '-5px',
+                        top: '-20px',
                         left: '50%',
                         transform: 'translateX(-50%)'
                       }}>
